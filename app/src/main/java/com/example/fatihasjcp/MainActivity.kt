3c41860b93635d02daffe24f3209826f423c1d51
mfatihy70 <mfatihyildiz1453@gmail.com>
2024-03-29 15:07:44 +0100

    added the circular progress bar

--- app/src/main/java/com/example/fatihasjcp/MainActivity.kt
+++ app/src/main/java/com/example/fatihasjcp/MainActivity.kt
@@ -8,10 +8,12 @@ import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -42,6 +44,7 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -51,6 +54,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalConfiguration
@@ -61,9 +66,10 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
@@ -247,7 +253,7 @@ fun ConstraintLayoutExample() {
     val constrains = ConstraintSet {
         val greenBox = createRefFor("greenBox")
         val redBox = createRefFor("redBox")
-        val guideline = createGuidelineFromTop(190.dp)
+        val guideline = createGuidelineFromTop(250.dp)
 
         constrain(greenBox) {
             top.linkTo(guideline)
@@ -263,7 +269,7 @@ fun ConstraintLayoutExample() {
             //width = Dimension.fillToConstraints
             height = Dimension.value(80.dp)
         }
-        createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Packed)
+        //createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Packed)
     }
     ConstraintLayout(
         constrains,
@@ -343,13 +349,56 @@ fun SimpleAnimation() {
     }
 }
 
+
+@Composable
+fun CircularProgressBar(
+    percentage: Float,
+    fontSize: TextUnit = 28.sp,
+    colour: Color = Color.Green,
+    radius: Dp = 50.dp,
+    strokeWidth: Dp = 8.dp,
+    animationDuration: Int = 1000,
+    animationDelay: Int = 0) {
+        var animationPlayed by remember { mutableStateOf(false) }
+        val currentPercentage = animateFloatAsState(
+            targetValue = if (animationPlayed) percentage else 0f,
+            animationSpec = tween(
+                durationMillis = animationDuration,
+                delayMillis = animationDelay
+            ), label = "")
+
+    LaunchedEffect (key1 = true){ animationPlayed = true }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ){
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = colour,
+                startAngle = -90f,
+                sweepAngle = 360 * currentPercentage.value,
+                useCenter = false,
+                style = Stroke(width = strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        Text(
+            text = (currentPercentage.value * 100).toInt().toString(),
+            fontSize = fontSize,
+            color = Color.Black,
+        )
+    }
+}
+
 @Composable
 fun MainFunc(){
-
     LazyLists()
     ConstraintLayoutExample()
 
     Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        Row { Box(contentAlignment = Alignment.CenterEnd,
+                modifier = Modifier.fillMaxSize().padding(30.dp)) {
+                CircularProgressBar(percentage = 0.6f) } }
         Row (horizontalArrangement = Arrangement.End,
             modifier = Modifier.fillMaxWidth()){ SimpleAnimation() }
         Row (modifier = Modifier.horizontalScroll(rememberScrollState())){
@@ -359,8 +408,8 @@ fun MainFunc(){
             ImageCard(painterID = guerillakermit,title = "Guerilla Kermit") }
 
         Row { StyledText() }
-        Row {ColorBox();ColorBox()}
-        Row {ColorBox();ColorBox()}
+        Row { ColorBox();ColorBox() }
+        Row { ColorBox();ColorBox() }
         Row { GreetInput() }
     }
 }
